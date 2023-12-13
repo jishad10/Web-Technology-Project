184251c7c009908cf908115d36f45113f8d9d7bd
Jahidul islam Shikdar <118066090+jishad10@users.noreply.github.com>
2023-12-13 22:20:27 +0600

    Library Management System

---
+++ Final Completed Full Project/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>welcome</title>
+    </head>
+
+    <body>
+        <?php 
+            header('location: views/home.php');
+        ?>
+    </body>
+
+</html>
